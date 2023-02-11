2186c96d6ba13e3d83b9ce8c09303edb0e38f5d1
ronentgreat <ronenh53@gmail.com>
2023-02-11 21:47:42 +0000

    done with introduction section

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -3,7 +3,7 @@ import "./header.styles.css";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header_nav">
+      <section className="header_nav">
         <a className="logo" href="/#">Bean Scene</a>
         <nav className="nav">
           <ul className="nav_list">
@@ -18,7 +18,15 @@ const Header = () => {
           <a className="register__signin" href="/#">Sign In</a>
           <a className="register__signup" href="/#">Sign Up</a>
         </div>
-      </div>
+      </section>
+      <section className="header__content">
+        <p className="header__content_paragraph-head">We’ve got your morning covered with</p>
+        <h1 className="header__content-head">Coffee</h1>
+        <p className="header__content_desc">It is best to start your day with a cup of coffee. Discover the
+          best flavours coffee you will ever have. We provide the best
+          for our customers.</p>
+        <a href="/#" className="header__content_button">Order Now</a>
+      </section>
       <div className="overlay"></div>
     </header>
   );
