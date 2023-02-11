f49f4d80a5141759849587b02d4e32f5c550f7a1
ronentgreat <ronenh53@gmail.com>
2023-02-11 00:18:39 +0000

    done with navbar and added background image

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,11 +1,27 @@
 import "./header.styles.css";
 
 const Header = () => {
-  return ( 
-    <header>
-      
+  return (
+    <header className="header">
+      <div className="header_nav">
+        <a className="logo" href="/#">Bean Scene</a>
+        <nav className="nav">
+          <ul className="nav_list">
+            <li className="nav_link"><a href="">Home</a></li>
+            <li className="nav_link"><a href="">Menu</a></li>
+            <li className="nav_link"><a href="">About Us</a></li>
+            <li className="nav_link"><a href="">Contact Us</a></li>
+          </ul>
+        </nav>
+
+        <div className="register">
+          <a className="register__signin" href="/#">Sign In</a>
+          <a className="register__signup" href="/#">Sign Up</a>
+        </div>
+      </div>
+      <div className="overlay"></div>
     </header>
-   );
+  );
 }
- 
+
 export default Header;
