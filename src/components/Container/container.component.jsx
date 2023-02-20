1f892f469ed20cda31914cc36a145ad43893f0be
ronentgreat <ronenh53@gmail.com>
2023-02-20 14:33:19 +0000

    styles first section in container component

--- src/components/Container/container.component.jsx
+++ src/components/Container/container.component.jsx
@@ -5,12 +5,12 @@ const Container = () => {
   return ( 
     <main className="container">
       <section className="info">
-      <div>
+      <div className="info__desc">
           <h1 className="info__header">Discover the best coffee</h1>
-          <p className="info__paragrahp">
+          <p className="info__paragraph">
             Bean Scene is a coffee shop that provides you with quality coffee that helps boost your productivity and helps build your mood. Having a cup of coffee is good, but having a cup of real coffee is greater. There is no doubt that you will enjoy this coffee more than others you have ever tasted.
           </p>
-          <a href="/#" className="info_btn">Learn More</a>
+          <a href="/#" className="info__btn">Learn More</a>
       </div>
         <img src={coffeeBeanImg} alt="coffee beans" />
       </section>
