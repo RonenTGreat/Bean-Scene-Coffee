fb3ce56ba9dab684a7e09f915115f2c4aade57d6
ronentgreat <ronenh53@gmail.com>
2023-02-20 14:08:55 +0000

    added container component and added structure to first section in the container

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/header.component'
+import Container from './components/Container/container.component'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Container />
     </div>
   )
 }
---
+++ src/components/Container/container.component.jsx
@@ -0,0 +1,21 @@
+import "./container.styles.css";
+import coffeeBeanImg from "../../assets/images/coffee-beans-flat.png";
+
+const Container = () => {
+  return ( 
+    <main className="container">
+      <section className="info">
+      <div>
+          <h1 className="info__header">Discover the best coffee</h1>
+          <p className="info__paragrahp">
+            Bean Scene is a coffee shop that provides you with quality coffee that helps boost your productivity and helps build your mood. Having a cup of coffee is good, but having a cup of real coffee is greater. There is no doubt that you will enjoy this coffee more than others you have ever tasted.
+          </p>
+          <a href="/#" className="info_btn">Learn More</a>
+      </div>
+        <img src={coffeeBeanImg} alt="coffee beans" />
+      </section>
+    </main>
+   );
+}
+ 
+export default Container;
