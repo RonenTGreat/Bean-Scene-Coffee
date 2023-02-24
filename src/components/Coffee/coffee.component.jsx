7700c50c1b5a124abb125f54afc10590d03f82a5
ronentgreat <ronenh53@gmail.com>
2023-02-24 19:11:01 +0000

    added coffee blast picture

--- src/components/Coffee/coffee.component.jsx
+++ src/components/Coffee/coffee.component.jsx
@@ -8,7 +8,7 @@ const Coffee = (props) => {
         <h2 className="coffee__order-header">{props.coffee_header}</h2>
         <p className="coffee__order-desc">Coffee 50% | Milk 50%</p>
         <p className="coffee__order-price">$8.50</p>
-        <a className="info__btn order_btn">Order Now</a>
+        <a href="/#" className="info__btn order_btn">Order Now</a>
       </div>
     </section>
   );
