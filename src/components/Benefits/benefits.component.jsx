8e2172251bdac28440d4f1cfbf4c6745643c0c37
ronentgreat <ronenh53@gmail.com>
2023-03-08 20:51:24 +0000

    added active state to button

--- src/components/Benefits/benefits.component.jsx
+++ src/components/Benefits/benefits.component.jsx
@@ -1,13 +1,26 @@
 import "./benefits.styles.css"
 
+import React, { useState } from "react";
+
 const Benefits = (props) => {
-  return (  
-    <section className="benefits__container">
-      <img className="benefits__container-img" src={props.img} alt="benefits" />
-      <h3 className="benefits__container-header">{props.header}</h3>
-      <p className="benefits__container-paragraph">{props.para}</p>
-    </section>
-  );
-}
+
  
-export default Benefits;
+    const [active, setActive] = useState(false);
+    const handleClick = () => {
+      setActive(!active);
+    }
+
+    return (
+      <button
+        className="benefits__container"
+        onClick={handleClick}
+        style={{ backgroundColor: active ? "#ffeed8" : "" }}
+      >
+        <img className="benefits__container-img" src={props.img} alt="benefits" />
+        <h3 className="benefits__container-header">{props.header}</h3>
+        <p className="benefits__container-paragraph">{props.para}</p>
+      </button>
+    );
+  }
+
+  export default Benefits;
